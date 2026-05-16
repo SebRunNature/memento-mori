1d52521febddccc25abbaee610ed8e525388dbee
Seb Run Nature <seb.salomon.lux@gmail.com>
2026-05-16 12:57:55 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Montre de Vie — Service Worker
 // Version du cache — incrémente pour forcer la mise à jour
-var CACHE_VERSION = 'montre-vie-v9';
+var CACHE_VERSION = 'montre-vie-v10';
 
 // Fichiers à mettre en cache pour le mode offline
 var CACHE_FILES = [
